4c9f94ab6a97bac2d70061fb10281cd17b7ce97e
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2020-01-06 11:05:57 -0800

    Set up proxy with middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require('newrelic');
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
@@ -8,3 +10,12 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 const port = 3000;
 app.listen(port, console.log(`listening on ${port}`));
+
+const options = {
+  target: 'http://localhost:3001/',
+  changeOrigin: true,
+  ws: true,
+};
+
+const apiProxy = proxy(options);
+app.use('/', apiProxy);
